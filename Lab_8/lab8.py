ef128d4ba2a9e05084b6bbb59a57964b695121f6
AdrianAlbrecht <155376@student.uwm.edu.pl>
2021-11-26 12:18:18 +0100

    uncomment lab8

--- Lab_8/lab8.py
+++ Lab_8/lab8.py
@@ -39,35 +39,35 @@ def filtruj(obraz, kernel, scale):
 
 #Zadanie 2
 print(ImageFilter.BLUR.filterargs)
-#obraz.show()
+obraz.show()
 obraz_blur = filtruj(obraz, [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]], ImageFilter.BLUR.filterargs[1])
-#obraz_blur.show()
+obraz_blur.show()
 obraz_blur.save(path+"blur.jpg")
 obraz_blur_filtr = obraz.filter(ImageFilter.BLUR)
 obraz_blur_filtr.save(path+"blur1.jpg")
-#ImageChops.difference(obraz_blur_filtr,obraz_blur).show()
+ImageChops.difference(obraz_blur_filtr,obraz_blur).show()
 
 #Zadanie 3.1
 print(ImageFilter.SHARPEN.filterargs)
 obraz_sharpen = filtruj(obraz, ((-2, -2, -2), (-2, 32, -2),( -2, -2, -2)), ImageFilter.SHARPEN.filterargs[1])
-#obraz_sharpen.show()
+obraz_sharpen.show()
 obraz_sharpen.save(path+"filtr1_1.jpg")
 obraz_sharpen_filtr = obraz.filter(ImageFilter.SHARPEN)
 obraz_sharpen_filtr.save(path+"filtr1_2.jpg")
-#ImageChops.difference(obraz_sharpen_filtr, obraz_sharpen).show()
+ImageChops.difference(obraz_sharpen_filtr, obraz_sharpen).show()
 
 #Zadanie 3.2
 print(ImageFilter.SMOOTH_MORE.filterargs)
 obraz_smooth_more = filtruj(obraz, ((1, 1, 1, 1, 1), (1, 5, 5, 5, 1),( 1, 5, 44, 5, 1),( 1, 5, 5, 5, 1),( 1, 1, 1, 1, 1)), ImageFilter.SMOOTH_MORE.filterargs[1])
-#obraz_smooth_more.show()
+obraz_smooth_more.show()
 obraz_smooth_more.save(path+'filtr2_1.jpg')
 obraz_smooth_more_filter = obraz.filter(ImageFilter.SMOOTH_MORE)
 obraz_smooth_more_filter.save(path+"filtr2_2.jpg")
-#ImageChops.difference(obraz_smooth_more_filter,obraz_smooth_more).show()
+ImageChops.difference(obraz_smooth_more_filter,obraz_smooth_more).show()
 
 #Zadanie 4
 obraz_emboss = obraz.filter(ImageFilter.EMBOSS)
-#obraz_emboss.show()
+obraz_emboss.show()
 obraz_emboss.save(path+"emboss.jpg")
 
 #Zadanie 5
@@ -76,12 +76,12 @@ obraz_L = obraz.convert("L")
 ImageFilter.EMBOSS.filterargs = (
     (3, 3), 1, 128, (-1,  0,  1, -2,  0,  2, -1,  0,  1))
 obraz_sobel_1 = obraz_L.filter(ImageFilter.EMBOSS)
-#obraz_sobel_1.show()
+obraz_sobel_1.show()
 obraz_sobel_1.save(path+"sobel1.jpg")
 ImageFilter.EMBOSS.filterargs = (
     (3, 3), 1, 128, (-1, -2, -1, 0,  0, 0, 1, 2, 1))
 obraz_sobel_2 = obraz_L.filter(ImageFilter.EMBOSS)
-#obraz_sobel_2.show()
+obraz_sobel_2.show()
 obraz_sobel_2.save(path+"sobel2.jpg")
 
 #Sobel1 jest wypukły, Sobel2 jest wklęsły
